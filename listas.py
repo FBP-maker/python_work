2327bd8b0c40dbf61e2bd29a8a1a6772d239393d
FBP-maker <filipe_bastos5@hotmail.com>
2021-12-16 19:46:58 -0300

    listas

---
+++ listas.py
@@ -0,0 +1,10 @@
+#Trabalhando com listas.
+linux_distribution = ['arch','majaro','debian','ubuntu']
+print(linux_distribution[0].title())
+print(linux_distribution[1].title())
+print(linux_distribution[2].title())
+print(linux_distribution[3].title())
+print(linux_distribution[-1].title())
+print(linux_distribution[-2].title())
+print(linux_distribution[-3].title())
+print(linux_distribution[-4].title())
